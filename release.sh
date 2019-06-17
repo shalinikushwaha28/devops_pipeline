342bb91aecd3e907cba367f912262ed40b478391
Ubuntu <kmaster@mathco-k8s-codex.fqfwxjvn3ekuppevgcutw2hc1a.bx.internal.cloudapp.net>
2019-06-17 09:53:51 +0000

    Incrementing version number to ./release.sh.1

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 # v1.0.0, v1.5.2, etc.
-versionLabel=v$1
+versionLabel=$0.1
 
 # establish branch and tag name variables
 masterBranch=master
