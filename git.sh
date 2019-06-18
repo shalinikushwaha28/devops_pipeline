0fe80f1ca45fb77173ce5ebe2c490bc18a79f8d9
SuneelMaurya <suneelmaurya25@gmail.com>
2019-06-18 03:34:06 +0000

    changes done in code

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@
 
 #Get the highest tag number
 VERSION=`git describe --abbrev=0 --tags`
-VERSION=${VERSION:-'v0.1-develop-v0.0'}
+VERSION=${VERSION:-'v0.0'}
 
 #Get number parts
 MAJOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
@@ -23,12 +23,9 @@ echo "Updating to $NEW_TAG"
 #Only tag if no tag already (would be better if the git describe command above could have a silent option)
 if [ -z "$NEEDS_TAG" ]; then
     echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
-    git tag $NEW_TAG
+    git tag $NEW_TAG   #tagging to git
+    git push --tags    #pushing tag to origin
 else
     echo "Already a tag on this commit"
 fi
 
-#Pushing tag to git 
-git push --tags
-#git push
-#git push origin --tags
